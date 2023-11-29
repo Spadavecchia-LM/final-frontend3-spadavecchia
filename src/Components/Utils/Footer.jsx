4ceb722a9800c90d194873afc4441635e12c68c8
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2023-11-29 20:49:05 -0300

    responsive del texto del footer

--- src/Components/Utils/Footer.jsx
+++ src/Components/Utils/Footer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
+      <p>
       Todos los derechos reservados. Examen Final de Frontend III de Leonardo
       Spadavecchia.
+      </p>
     </div>
   );
 };
